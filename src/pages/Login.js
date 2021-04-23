cdb5d6dd6cfe66b16b8d745c5ea70797a5a432c0
Stephen Cheng <zintaen@gmail.com>
2021-04-23 23:25:00 +0700

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,55 @@
+import { Form, Input, Button, Checkbox } from "antd";
 import { Link } from "react-router-dom";
+
+import "./Login.scss";
+
 function Login() {
   return (
-    <div>
-      <Link to="/register">Register</Link>
+    <div className="login-container">
+      <div className="title">WTA</div>
+      <div className="content">Welcome to WTA</div>
+      <div className="login">Login in. To see it in action.</div>
+      <input
+        type="text"
+        id="register_email"
+        className="ant-input mt-15"
+        placeholder="e-mail"
+      />
+      <input
+        action="click"
+        id="register_password"
+        type="password"
+        className="ant-input mt-15"
+        aria-autocomplete="list"
+        placeholder="password"
+      />
+      <Button
+        type="primary"
+        htmlType="submit"
+        className="login-form-button green mt-15"
+      >
+        Log in
+      </Button>
+      <div className="login mt-15">
+        <a href="#" className="login">
+          Do not have an account?
+        </a>
+      </div>
+      <Button
+        type="primary"
+        htmlType="submit"
+        className="login-form-button white mt-15"
+      >
+        <Link to="/register">Sign Up</Link>
+      </Button>
+      <Button
+        type="primary"
+        htmlType="submit"
+        className="login-form-button yellow mt-15"
+      >
+        Android app download
+      </Button>
+      <div className="login mt-15">WTA &copy; 2018</div>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,62 @@
+import { Form, Input, Button, Checkbox } from "antd";
+import { Link } from "react-router-dom";
+
+import "./Register.scss";
+
 function Register() {
-    return <div>Register</div>;
+  return (
+    <div className="register-container">
+      <div className="title">WTA</div>
+      <div className="content">Register to WTA</div>
+      <div className="login">Create account to see it in action.</div>
+      <input
+        type="text"
+        id="register_email"
+        className="ant-input mt-15"
+        placeholder="e-mail"
+      />
+      <input
+        action="click"
+        id="register_password"
+        type="password"
+        className="ant-input mt-15"
+        aria-autocomplete="list"
+        placeholder="password"
+      />
+      <input
+        action="click"
+        id="register_password"
+        type="password"
+        className="ant-input mt-15"
+        aria-autocomplete="list"
+        placeholder="URL"
+      />
+      <div className="login mt-15">
+        <Checkbox>I accept the Terms and Conditions.</Checkbox>
+      </div>
+      <Button
+        type="primary"
+        htmlType="submit"
+        className="login-form-button green mt-15"
+      >
+        Sign Up
+      </Button>
+      <div className="login mt-15">
+        <a href="#" className="login">
+          Already have an account?
+        </a>
+      </div>
+
+      <Button
+        type="primary"
+        htmlType="submit"
+        className="login-form-button white mt-15"
+      >
+        <Link to="/login">Login</Link>
+      </Button>
+
+      <div className="login mt-15">WTA &copy; 2018</div>
+    </div>
+  );
 }
 export default Register;
